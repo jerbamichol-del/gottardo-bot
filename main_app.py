987de658195857fd10c8082ac78b714d0f3e3769
jerbamichol-del <jerbamichol@gmail.com>
2026-02-09 22:12:47 +0100

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -2001,3 +2001,4 @@ if "res" in st.session_state:
             p3, p4 = st.columns(2)
             p3.metric("Fruite", f"{safe_float_val(par.get('fruite', 0)):.2f}")
             p4.metric("Saldo", f"{safe_float_val(par.get('saldo', 0)):.2f}")
+
